bd3c5d4984a496d5a7d095b4b17e95b47808ab6e
Perry Roper <perryroper@gmail.com>
2014-08-18 20:13:58 +0100

    Initial rebuild.

---
+++ Gruntfile.js
@@ -0,0 +1,204 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Use jit-grunt to only load necessary tasks for each invocation of grunt.
+    require('jit-grunt')(grunt, {
+        // useminPrepare is a task of usemin.
+        'useminPrepare': 'grunt-usemin',
+    });
+
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        config: {
+            app: 'app',
+            dist: 'dist',
+            htmlmin: {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            dev: [
+                'compass:watch'
+            ]
+        },
+        connect: {
+            options: {
+                port: process.env.PORT || 9000
+            },
+            dev: {
+                options: {
+                    base: '<%= config.app %>'
+                }
+            },
+            dist: {
+                options: {
+                    base: '<%= config.dist %>'
+                }
+            }
+        },
+        open: {
+            server: {
+                url: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        compass: {
+            options: {
+                sassDir: '<%= config.app %>/sass',
+                cssDir: '<%= config.app %>/styles',
+                imagesDir: '<%= config.app %>/images',
+                generatedImagesDir: '<%= config.app %>/images/generated',
+                fontsDir: '<%= config.app %>/fonts',
+                relativeAssets: true,
+                debugInfo: false
+            },
+            dev: {
+                options: {}
+            },
+            force: {
+                options: {
+                    force: true
+                }
+            },
+            watch: {
+                options: {
+                    watch: true
+                }
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= config.dist %>/*',
+                        '!<%= config.dist %>/.git*'
+                    ]
+                }]
+            },
+            dev: [
+                '<%= config.app %>/styles',
+                '<%= config.app %>/images/generated'
+            ]
+        },
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= config.dist %>/styles/**/*.css',
+                        '<%= config.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
+                        '!<%= config.dist %>/images/inline_html_images/**/*'
+                    ]
+                }
+            }
+        },
+        useminPrepare: {
+            html: '<%= config.app %>/index.html',
+            options: {
+                dest: '<%= config.dist %>'
+            }
+        },
+        usemin: {
+            html: ['<%= config.dist %>/{,*/}*.html'],
+            css: ['<%= config.dist %>/styles/{,*/}*.css'],
+            options: {
+                dirs: ['<%= config.dist %>']
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    src: '<%= config.dist %>/images/**/*.{png,jpg,jpeg}'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    src: '<%= config.dist %>/images/**/*.svg'
+                }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: '<%= config.htmlmin %>',
+                files: [{
+                    expand: true,
+                    src: '<%= config.dist %>/index.html'
+                }]
+            }
+        },
+        // Put files not handled in other tasks here
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= config.app %>',
+                        dest: '<%= config.dist %>',
+                        src: [
+                            'index.html',
+                            '*.{ico,png,txt}',
+                            'images/**/*.{png,jpg,jpeg,gif,webp,svg}',
+                            '!images/*/sprite-files/*',
+                            'fonts/**/*',
+                            '!fonts/**/originals/*',
+                            '!fonts/**/*.json'
+                        ]
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                compress: {
+                    drop_console: true
+                }
+            }
+        },
+    });
+
+    grunt.registerTask('default', 'dev');
+
+    grunt.registerTask('dev', function (locale) {
+        grunt.task.run([
+            'clean:dev',
+            'compass:dev',
+            'connect:dev',
+            'open',
+            'concurrent:dev'
+        ]);
+    });
+
+    grunt.registerTask('build', function (locale) {
+        grunt.task.run([
+            'clean',
+            'compass:dev',
+            'useminPrepare',
+            'copy:dist',
+            'concat:generated',
+            'imagemin',
+            'svgmin',
+            'cssmin',
+            'uglify',
+            'rev',
+            'usemin',
+            'htmlmin'
+        ]);
+    });
+};
+
---
+++ Makefile
@@ -0,0 +1,22 @@
+BASE_DIR=$(CURDIR)
+DIST_DIR=$(BASEDIR)/dist
+
+S3_BUCKET=perry.io
+
+build:
+	grunt build
+
+s3_upload:
+	s3cmd sync $(DIST_DIR)/ s3://$(S3_BUCKET) --acl-public --delete-removed
+
+deploy:
+	@status=$$(git status --porcelain); \
+	if test "x$${status}" = x; then \
+		git pull; \
+		git push; \
+		make build; \
+		make s3_upload; \
+	else \
+		echo Please commit all changes before deploying. >&2; \
+	fi
+
