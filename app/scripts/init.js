f9581ce511afcd7d6cba2d34a2b118e7232288dd
Perry Roper <perryroper@gmail.com>
2015-10-13 19:54:15 +0100

    Remove fonts and unused JS.

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,9 +1,5 @@
 (function() {
     document.addEventListener('DOMContentLoaded', function() {
-        var setBodyClass = function () {
-            document.body.className += ' images-loaded';
-        };
-
         var bLazy = new Blazy();
     });
 })();
